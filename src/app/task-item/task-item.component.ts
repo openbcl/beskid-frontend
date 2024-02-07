94f1aafab3ee109216142a2a4b8ec7ab74aedfea
Robert Weiße <rweisse@users.noreply.github.com>
2024-02-07 16:25:23 +0100

    Minor changes

--- src/app/task-item/task-item.component.ts
+++ src/app/task-item/task-item.component.ts
@@ -1,13 +1,12 @@
 import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { CardModule } from 'primeng/card';
 import { Task } from '../store/task';
 
 @Component({
   selector: 'be-task-item',
   standalone: true,
-  imports: [DatePipe, CardModule, RouterLink],
+  imports: [DatePipe, CardModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.scss'
 })
