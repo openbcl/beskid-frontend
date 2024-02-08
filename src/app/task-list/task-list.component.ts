59d134ffea1d0fc42a44593333b46844cbc09d75
Robert Weiße <rweisse@users.noreply.github.com>
2024-02-08 11:13:49 +0100

    Implemented routerLinkActive

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -4,15 +4,16 @@ import { Store } from '@ngrx/store';
 import { tasks } from '../store/task.selector';
 import { AsyncPipe } from '@angular/common';
 import { TaskItemComponent } from "../task-item/task-item.component";
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
     selector: 'be-task-list',
     standalone: true,
     templateUrl: './task-list.component.html',
     styleUrl: './task-list.component.scss',
-    imports: [AsyncPipe, TaskItemComponent, RouterLink, ButtonModule]
+    imports: [AsyncPipe, TaskItemComponent, RouterLink, RouterLinkActive, ButtonModule, RippleModule]
 })
 export class TaskListComponent {
 
