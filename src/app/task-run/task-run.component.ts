57f164d373d248250af388abf9cee3eed612e330
Robert Weiße <rweisse@users.noreply.github.com>
2024-03-08 10:47:18 +0100

    Implemented runTask()

--- src/app/task-run/task-run.component.ts
+++ src/app/task-run/task-run.component.ts
@@ -8,6 +8,15 @@ import { DropdownModule } from 'primeng/dropdown';
 import { models } from '../store/model.selector';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { findModels } from '../store/model.actions';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Subject, filter } from 'rxjs';
+import { Model } from '../store/model';
+import { runTask } from '../store/task.actions';
+
+interface ResolutionItem {
+  name: string;
+  value: number;
+}
 
 @Component({
   selector: 'be-task-run',
@@ -20,20 +29,46 @@ export class TaskRunComponent implements OnInit {
   @Input({ required: true }) task: Task | undefined;
 
   models$ = this.store.select(models);
+  resolutions$ = new Subject<any[]>()
 
   form = this.fb.group({
-    selectedModel: this.fb.control(null, { validators: [Validators.required]}),
-    selectedResolution: this.fb.control(null, { validators: [Validators.required]}),
+    selectedModel: this.fb.control({}, { validators: [Validators.required]}),
+    selectedResolution: this.fb.control({}, { validators: [Validators.required]}),
   });
 
   constructor(
     private store: Store,
     private fb: FormBuilder
-  ) {}
+  ) {
+    this.models$.pipe(takeUntilDestroyed(), filter(models => !!models?.length)).subscribe(models => this.updateSelectedModel(models[0]));
+  }
 
   ngOnInit(): void {
     this.store.dispatch(findModels());
   }
 
+  updateSelectedModel(model: Model) {
+    this.form.controls.selectedModel.setValue(model);
+    this.updateResolutions(model.resolutions);
+  }
 
+  updateResolutions(resolutions: number[]) {
+    const resolutionItem = (resolution: number): ResolutionItem => ({
+      name: `${resolution}`, value: resolution
+    });
+    this.resolutions$.next(resolutions.map(resolution => resolutionItem(resolution)));
+    this.form.controls.selectedResolution.setValue(resolutionItem(resolutions[0]));
+  }
+
+  changeModel(event: { value: Model }) {
+    this.updateResolutions(event.value.resolutions);
+  }
+
+  runTask() {
+    this.store.dispatch(runTask({
+      taskId: this.task?.id!,
+      modelId: (this.form.value.selectedModel as Model).id,
+      resolution: (this.form.value.selectedResolution as ResolutionItem).value
+    }));
+  }
 }
