8f263015a2192e818e86cd8b9fee8c2abec9c7bd
Robert Weiße <rweisse@users.noreply.github.com>
2024-03-18 10:42:38 +0100

    Finished welcome page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FieldsetModule } from 'primeng/fieldset';
+import { MessagesModule } from 'primeng/messages';
+import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { tasks } from '../store/task.selector';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'be-welcome',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, FieldsetModule, MessagesModule, TagModule, ButtonModule, RouterLink],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
 })
 export class WelcomeComponent {
 
+  tasks$ = this.store.select(tasks);
+
+  constructor(private store: Store) {}
 }
