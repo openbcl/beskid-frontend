199997156e00724164af933d65530026b1820b6b
Robert Weiße <rweisse@users.noreply.github.com>
2024-03-18 10:46:39 +0100

    Show create-task button on welcome page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -4,20 +4,13 @@ import { MessagesModule } from 'primeng/messages';
 import { TagModule } from 'primeng/tag';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { tasks } from '../store/task.selector';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'be-welcome',
   standalone: true,
-  imports: [AsyncPipe, FieldsetModule, MessagesModule, TagModule, ButtonModule, RouterLink],
+  imports: [FieldsetModule, MessagesModule, TagModule, ButtonModule, RouterLink],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
 })
 export class WelcomeComponent {
-
-  tasks$ = this.store.select(tasks);
-
-  constructor(private store: Store) {}
 }
